397a669dc24303548d65ca20a509f416c0530e37
BT_1N3_08@BKTECH.LOCAL <BT_1N3_08@BKTECH.LOCAL>
2018-09-24 10:11:56 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Scanner input = new Scanner(System.in);
+	System.out.println("Hey what is your name?");
+	String name = input.next();
+	System.out.println("Hello "+  name + ", nice to meet you!");
+
+	System.out.println("What is your age?");
+	String age= input.next();
+	System.out.println("Wow you are only "+ age + " years young?");
+
+
+	System.out.println("Wow its your turn to guess my age!");
+	int counter = 0;
+	
+	String age = input.next();
+	if(age=)
+
+
+}
